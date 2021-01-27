a6867ae45b4522ab9c2220d8e14bf9cfde445009
Jhonnatan <jsalvatierra@canvia.com>
2021-01-27 13:20:23 -0500

    Payments, Email and File Uploads

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,15 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
+
 
 if (mapBox) {
     const locations = JSON.parse(document.getElementById('map').dataset.locations);
@@ -57,5 +60,14 @@ if (userPasswordForm) {
         document.getElementById('password-current').value = '';
         document.getElementById('password').value = '';
         document.getElementById('password-confirm').value = '';
+    });
+}
+
+if (bookBtn) {
+    bookBtn.addEventListener('click', e => {
+        e.target.textContent = 'Processing...';
+        const { tourId } = e.target.dataset;
+        //const tourId = e.target.dataset.tourId;
+        bookTour(tourId);
     });
 }
---
+++ public/js/stripe.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_51IDe6qIiq7RqlXCok7DB4UBaibyVoXk4Rph33XhSOtBNOY9XzQRC6Ymudxcvz87QhhZGGtx6zrhobevhCL2799fC00AFIu4ROi');
+
+export const bookTour = async tourId => {
+    try {
+        //1 Get checkout session from API
+        const session = await axios(`http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`);
+        console.log(session);
+
+        //2 Create checkout form + charge credit card
+        await stripe.redirectToCheckout({
+            sessionId: session.data.session.id
+        });
+    } catch (error) {
+        console.log(error);
+        showAlert('error', error);
+    }
+};
