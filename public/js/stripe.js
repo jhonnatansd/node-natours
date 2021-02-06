ac307e3c7702d8798f8359ac4197500488e44b51
Jhonnatan <jsalvatierra@canvia.com>
2021-02-06 14:29:41 -0500

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,7 +5,7 @@ const stripe = Stripe('pk_test_51IDe6qIiq7RqlXCok7DB4UBaibyVoXk4Rph33XhSOtBNOY9X
 export const bookTour = async tourId => {
     try {
         //1 Get checkout session from API
-        const session = await axios(`http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`);
+        const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
         console.log(session);
 
         //2 Create checkout form + charge credit card
