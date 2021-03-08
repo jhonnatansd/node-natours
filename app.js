3a379a506c3082ec06c482699080669ca880bf55
Jhonnatan <jsalvatierra@canvia.com>
2021-03-08 17:33:13 -0500

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +29,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Global Middlewares
+//Implement cors
+app.use(cors());
+//Cors for a specific domain
+// app.use(cors({
+//     origin: 'https://www.natours.com'
+// }));
+app.options('*', cors());
+
 //Set security HTTP headers
 app.use(helmet())
 
